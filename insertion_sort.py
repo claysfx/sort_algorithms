439db1b2336d9da8578ef0c92ec0e6c5219c9d03
Cleydson Junior <csfj@academico.ufpb.br>
2023-11-11 02:16:42 -0300

    organização

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,18 +1,14 @@
 def insertion_sort(elementos):
-  for i in range(1, len(elementos)):
-      key = elementos[i]  # o elemento que será ordenado
-      j = i - 1
+    for i in range(1, len(elementos)):
+        key = elementos[i]  # o elemento que será ordenado
+        j = i - 1
 
-      # "empurrando" os elementos para a direita
-      while j >= 0 and key < elementos[j]:
-          elementos[j + 1] = elementos[j]
-          j -= 1
+        # "empurra" os elementos para a direita
+        while j >= 0 and key < elementos[j]:
+            elementos[j + 1] = elementos[j]
+            j -= 1
 
-      # inserindo o elemento na posição adequada
-      elementos[j + 1] = key
+            # insere o elemento na posição adequada
+            elementos[j + 1] = key
 
-# Exemplo de uso:
-elementos = [64, 25, 12, 22, 11]
-insertion_sort(elementos)
-print("Lista ordenada:")
-print(elementos)
+    return elementos
--- selection_sort.py
+++ selection_sort.py
@@ -2,10 +2,10 @@ def selection_sort(elementos):
   n = len(elementos)
 
   for i in range(n - 1):
-    # o índice da iteração será o menor índice da lista que ainda não foi preenchido
+      # o índice da iteração será o menor índice da lista que ainda não foi preenchido
       min_index = i
 
-          # seleção do menor elemento ainda não preenchido na lista ordenada 
+      # seleção do menor elemento ainda não preenchido na lista ordenada 
       for j in range(i + 1, n):
           if elementos[j] < elementos[min_index]:
               min_index = j
@@ -13,8 +13,4 @@ def selection_sort(elementos):
       # troca do elemento atual com o menor elemento encontrado
       elementos[i], elementos[min_index] = elementos[min_index], elementos[i]
 
-# Exemplo de uso:
-elementos = [64, 25, 12, 22, 11]
-selection_sort(elementos)
-print("Lista ordenada:")
-print(elementos)
+  return elementos
